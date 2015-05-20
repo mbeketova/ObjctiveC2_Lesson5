9c474e00c98ec8a007b8755bdedd5a44ae0d7f48
Mariya <mbeketova@rambler.ru>
2015-05-20 18:49:21 +0300

    Initial Commit

---
+++ ObjctiveC2_Lesson5/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ObjctiveC2_Lesson5
+//
+//  Created by Admin on 20.05.15.
+//  Copyright (c) 2015 Mariya Beketova. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
